c6e07a8db775fa3b81bca2790d16334bf274db59
Ali Chraghi <chraghiali1@gmail.com>
2022-05-10 06:08:38 +0430

    remove useless doc comments (repeated in FreeType documentions)

--- src/face.zig
+++ src/face.zig
@@ -25,7 +25,6 @@ pub fn init(handle: c.FT_Face) Face {
     };
 }
 
-/// Call `attachStream` to attach a file by its path
 pub fn attachFile(self: Face, path: []const u8) Error!void {
     return self.attachStream(.{
         .flags = .{ .path = true },
@@ -33,7 +32,6 @@ pub fn attachFile(self: Face, path: []const u8) Error!void {
     });
 }
 
-/// Call `attachStream` to attach additional information from a bytes slice
 pub fn attachMemory(self: Face, bytes: []const u8) Error!void {
     return self.attachStream(.{
         .flags = .{ .memory = true },
@@ -41,148 +39,35 @@ pub fn attachMemory(self: Face, bytes: []const u8) Error!void {
     });
 }
 
-/// 'Attach' data to a face object. Normally, this is used to read
-/// additional information for the face object. For example, you can
-/// attach an AFM or PFM file that comes with a Type 1 font to get the kerning
-/// values and other metrics.
 pub fn attachStream(self: Face, args: OpenArgs) Error!void {
     return checkError(c.FT_Attach_Stream(self.handle, &args.toCInterface()));
 }
 
-/// Request the nominal size in points
-///
-/// `char_width`:
-///   The nominal width, in 26.6 fractional points.
-///
-/// `char_height`:
-///   The nominal height, in 26.6 fractional points.
-///
-/// `horz_resolution`:
-///   The horizontal resolution in dpi.
-///
-/// `vert_resolution`:
-///   The vertical resolution in dpi.
-///
-/// NOTE: While this function allows fractional points as input values, the
-/// resulting ppem value for the given resolution is always rounded to the
-/// nearest integer.
-///
-/// If either the character width or height is zero, it is set equal to
-/// the other value.
-///
-/// If either the horizontal or vertical resolution is zero, it is set
-/// equal to the other value.
-///
-/// A character width or height smaller than 1pt is set to `1pt`; if both
-/// resolution values are zero, they are set to `72dpi`.
-///
-/// Don't use this function if you are using the FreeType cache API.
 pub fn setCharSize(self: Face, pt_width: i32, pt_height: i32, horz_resolution: u16, vert_resolution: u16) Error!void {
     return checkError(c.FT_Set_Char_Size(self.handle, pt_width, pt_height, horz_resolution, vert_resolution));
 }
 
-/// Request the nominal size in pixels
-///
-/// `pixel_width`:
-///   The nominal width, in pixels.
-/// `pixel_height`:
-///   The nominal height, in pixels.
-///
-/// NOTE: You should not rely on the resulting glyphs matching or being
-/// constrained to this pixel size. Refer to `FT_Request_Size` to
-/// understand how requested sizes relate to actual sizes.
-///
-/// Don't use this function if you are using the FreeType cache API.
 pub fn setPixelSizes(self: Face, pixel_width: u32, pixel_height: u32) Error!void {
     return checkError(c.FT_Set_Pixel_Sizes(self.handle, pixel_width, pixel_height));
 }
 
-/// Load a glyph into the glyph slot of a face object
-///
-/// `index`:
-/// The index of the glyph in the font file.  For CID-keyed fonts
-/// either in PS or in CFF format) this argument specifies the CID
-/// value.
-///
-/// `load_flags`:
-/// A flag indicating what to load for this glyph. The `LoadFlags`
-/// can be used to control the glyph loading process (e.g.,
-/// whether the outline should be scaled, whether to load bitmaps or
-/// not, whether to hint the outline, etc).
-///
-/// NOTE: For proper scaling and hinting, the active `FT_Size` object owned by
-/// the face has to be meaningfully initialized by calling
-/// `setCharSize` before this function, for example. The loaded
-/// glyph may be transformed. See `setTransform` for the details.
-///
-/// For subsetted CID-keyed fonts,`FT_Err_Invalid_Argument` is returned
-/// for invalid CID values (this is, for CID values that don't have a
-/// corresponding glyph in the font). See the discussion of the
-/// `FT_FACE_FLAG_CID_KEYED` flag for more details.
-///
-/// If you receive `GlyphTooBig` error, try getting the glyph outline
-/// at EM size, then scale it manually and fill it as a graphics
-/// operation.
 pub fn loadGlyph(self: Face, index: u32, flags: LoadFlags) Error!void {
     return checkError(c.FT_Load_Glyph(self.handle, index, flags.toBitFields()));
 }
 
-/// Load a glyph into the glyph slot of a face object, accessed by its
-/// character code
 pub fn loadChar(self: Face, char: u32, flags: LoadFlags) Error!void {
     return checkError(c.FT_Load_Char(self.handle, char, flags.toBitFields()));
 }
 
-/// Set the transformation that is applied to glyph images when they are
-/// loaded into a glyph slot through `loadGlyph`.
-///
-/// NOTE: This function is provided as a convenience, but keep in mind that
-/// `Matrix` coefficients are only 16.16 fixed-point values, which can
-/// limit the accuracy of the results. Using floating-point computations
-/// to perform the transform directly in client code instead will always
-/// yield better numbers.
-///
-/// The transformation is only applied to scalable image formats after the
-/// glyph has been loaded. It means that hinting is unaltered by the
-/// transformation and is performed on the character size given in the
-/// last call to `setCharSize` or `setPixelSize`.
-///
-/// Note that this also transforms the `face.glyph.advance` field, but
-/// **not** the values in `face.glyph.metrics`.
 pub fn setTransform(self: Face, matrix: *Matrix, delta: *Vector) Error!void {
     return c.FT_Set_Transform(self.handle, @ptrCast([*c]c.FT_Matrix, matrix), @ptrCast([*c]c.FT_Vector, delta));
 }
 
-/// Return the glyph index of a given character code.
-/// This function uses the currently selected charmap to do the mapping.
-///
-/// NOTE:
-/// If you use FreeType to manipulate the contents of font files
-/// directly, be aware that the glyph index returned by this
-/// function doesn't always correspond to the internal indices
-/// used within the file. This is done to ensure that
-/// value 0 always corresponds to the ‘missing glyph’.
-/// If the first glyph is not named ‘.notdef’, then
-/// for Type `1` and Type `42` fonts, ‘.notdef’ will be moved
-/// into the glyph ID 0 position, and whatever
-/// was there will be moved to the position ‘.notdef’ had.
-/// For Type `1` fonts, if there is no ‘.notdef’ glyph
-/// at all, then one will be created at index `0` and whatever
-/// was there will be moved to the last index – Type `42` fonts
-/// are considered invalid under this condition.
 pub fn getCharIndex(self: Face, index: u32) ?u32 {
     var i = c.FT_Get_Char_Index(self.handle, index);
     return if (i == 0) null else i;
 }
 
-/// Return the kerning vector between two glyphs of the same face.
-/// Only horizontal layouts (left-to-right & right-to-left) are
-/// supported by this method. Other layouts, or more sophisticated
-/// kernings, are out of the scope of this API function – they can be
-/// implemented through format-specific interfaces.
-/// Kerning for OpenType fonts implemented in a ‘GPOS’ table is not supported;
-/// use FT_HAS_KERNING to find out whether a font has data that
-/// can be extracted with FT_Get_Kerning.
 pub fn getKerning(self: Face, left_char_index: u32, right_char_index: u32, mode: KerningMode) Error!Vector {
     var vec = std.mem.zeroes(Vector);
     try checkError(c.FT_Get_Kerning(self.handle, left_char_index, right_char_index, @enumToInt(mode), @ptrCast([*c]c.FT_Vector, &vec)));
--- src/library.zig
+++ src/library.zig
@@ -26,7 +26,6 @@ pub fn deinit(self: Library) void {
     };
 }
 
-/// Call `openFace` to open a font by its path.
 pub fn newFace(self: Library, path: []const u8, face_index: i32) Error!Face {
     return self.openFace(.{
         .flags = .{ .path = true },
@@ -34,9 +33,6 @@ pub fn newFace(self: Library, path: []const u8, face_index: i32) Error!Face {
     }, face_index);
 }
 
-/// Call `openFace` to open a font from a bytes slice
-///
-/// NOTE: Don't deallocate the memory before calling `Face.deinit()`
 pub fn newFaceMemory(self: Library, bytes: []const u8, face_index: i32) Error!Face {
     return self.openFace(.{
         .flags = .{ .memory = true },
@@ -44,34 +40,18 @@ pub fn newFaceMemory(self: Library, bytes: []const u8, face_index: i32) Error!Fa
     }, face_index);
 }
 
-/// Create a face object from a given resource described by `OpenArgs`
-///
-/// `face_index` field holds two different values.
-/// Bits `0`-`15` are the index of the face in the font file (starting with `0`).
-/// Set it to `0` if there is only one face in the font file.
-/// [*Since `2.6.1`*] Bits `16`-`30` are relevant to GX and OpenType variation
-/// fonts only, specifying the named instance index for the current face
-/// index (starting with value `1`; value `0` makes FreeType ignore named
-/// instances).  For non-variation fonts, bits `16`-`30` are ignored.
-/// Assuming that you want to access the third named instance in face `4`,
-/// `face_index` should be set to `0x00030004`.  If you want to access
-/// face `4` without variation handling, simply set *face_index* to value `4`
 pub fn openFace(self: Library, args: OpenArgs, face_index: i32) Error!Face {
     var face = mem.zeroes(c.FT_Face);
     try checkError(c.FT_Open_Face(self.handle, &args.toCInterface(), face_index, &face));
     return Face.init(face);
 }
 
-/// Create a new stroker object
 pub fn newStroker(self: Library) Error!Stroker {
     var stroker = mem.zeroes(c.FT_Stroker);
     try checkError(c.FT_Stroker_New(self.handle, &stroker));
     return Stroker.init(stroker);
 }
 
-/// Change filter applied to LCD decimated bitmaps,
-/// like the ones used when calling `Glyph.Slot.render` with
-/// `.render_mode_lcd` or `.render_mode_lcd_v` flags
 pub fn setLcdFilter(self: Library, lcd_filter: LcdFilter) Error!void {
     return checkError(c.FT_Library_SetLcdFilter(self.handle, @enumToInt(lcd_filter)));
 }
