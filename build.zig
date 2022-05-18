74fe370536831ab9126de3aee82751a1d3155602
Ali Chraghi <chraghiali1@gmail.com>
2022-05-18 13:17:38 +0430

    reduce code size

--- build.zig
+++ build.zig
@@ -76,161 +76,78 @@ fn thisDir() []const u8 {
 fn buildFreeType(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget, custom_config_path: ?[]const u8, comptime options: Options) !*std.build.LibExeObjStep {
     const lib = b.addStaticLibrary("freetype", ft_root ++ "/src/base/ftbase.c");
     lib.defineCMacro("FT2_BUILD_LIBRARY", "1");
-    lib.defineCMacro("HAVE_UNISTD_H", "1");
-    lib.defineCMacro("HAVE_FCNTL_H", "1");
     lib.setBuildMode(mode);
     lib.setTarget(target);
     lib.linkLibC();
     lib.addIncludePath(ft_root ++ "/include");
-
-    if (custom_config_path) |path| {
-        lib.addIncludeDir(path);
-    }
+    if (custom_config_path) |path| lib.addIncludeDir(path);
 
     var sources = std.ArrayList([]const u8).init(b.allocator);
     defer sources.deinit();
 
-    inline for (freetype_base_sources) |source| {
+    inline for (freetype_base_sources) |source|
         try sources.append(ft_root ++ source);
-    }
 
     if (options.bdf) {
         try sources.append(ft_root ++ "/src/base/ftbdf.c");
         try sources.append(ft_root ++ "/src/bdf/bdf.c");
     }
-
     if (options.cff) {
-        if (!options.sfnt) {
-            @compileError("CFF requires SFNT module");
-        }
-        if (!options.pshinter) {
-            @compileError("CFF requires PSHinter module");
-        }
-        if (!options.psnames) {
-            @compileError("CFF requires PSNames module");
-        }
+        if (!options.sfnt) @compileError("CFF requires SFNT module");
+        if (!options.pshinter) @compileError("CFF requires PSHinter module");
+        if (!options.psnames) @compileError("CFF requires PSNames module");
         try sources.append(ft_root ++ "/src/cff/cff.c");
     }
-
     if (options.type1cid) {
-        if (!options.psaux) {
-            @compileError("Type1CID requires PSAux module");
-        }
-        if (!options.pshinter) {
-            @compileError("Type1CID requires PSHinter module");
-        }
-        if (!options.psnames) {
-            @compileError("Type1CID requires PSNames module");
-        }
+        if (!options.psaux) @compileError("Type1CID requires PSAux module");
+        if (!options.pshinter) @compileError("Type1CID requires PSHinter module");
+        if (!options.psnames) @compileError("Type1CID requires PSNames module");
         try sources.append(ft_root ++ "/src/base/ftcid.c");
         try sources.append(ft_root ++ "/src/cid/type1cid.c");
     }
-
-    if (options.pcf) {
-        try sources.append(ft_root ++ "/src/pcf/pcf.c");
-    }
-
+    if (options.pcf) try sources.append(ft_root ++ "/src/pcf/pcf.c");
     if (options.pfr) {
         try sources.append(ft_root ++ "/src/base/ftpfr.c");
         try sources.append(ft_root ++ "/src/pfr/pfr.c");
     }
-
-    if (options.sfnt) {
-        try sources.append(ft_root ++ "/src/sfnt/sfnt.c");
-    }
-
+    if (options.sfnt) try sources.append(ft_root ++ "/src/sfnt/sfnt.c");
     if (options.truetype) {
-        if (!options.sfnt) {
-            @compileError("TrueType requires SFNT module");
-        }
-        if (!options.psnames) {
-            @compileError("TrueType requires PSNames module");
-        }
+        if (!options.sfnt) @compileError("TrueType requires SFNT module");
+        if (!options.psnames) @compileError("TrueType requires PSNames module");
         try sources.append(ft_root ++ "/src/truetype/truetype.c");
     }
-
     if (options.type1) {
-        if (!options.psaux) {
-            @compileError("Type1 requires PSAux module");
-        }
-        if (!options.pshinter) {
-            @compileError("Type1 requires PSHinter module");
-        }
-        if (!options.psnames) {
-            @compileError("Type1 requires PSNames module");
-        }
+        if (!options.psaux) @compileError("Type1 requires PSAux module");
+        if (!options.pshinter) @compileError("Type1 requires PSHinter module");
+        if (!options.psnames) @compileError("Type1 requires PSNames module");
         try sources.append(ft_root ++ "/src/base/fttype1.c");
         try sources.append(ft_root ++ "/src/base/ftfstype.c");
         try sources.append(ft_root ++ "/src/type1/type1.c");
     }
-
     if (options.type42) {
-        if (!options.truetype) {
-            @compileError("Type42 requires TrueType module");
-        }
+        if (!options.truetype) @compileError("Type42 requires TrueType module");
         try sources.append(ft_root ++ "/src/type42/type42.c");
     }
-
     if (options.winfnt) {
         try sources.append(ft_root ++ "/src/base/ftwinfnt.c");
         try sources.append(ft_root ++ "/src/winfonts/winfnt.c");
     }
 
-    if (options.autofit) {
-        try sources.append(ft_root ++ "/src/autofit/autofit.c");
-    }
+    if (options.autofit) try sources.append(ft_root ++ "/src/autofit/autofit.c");
+    if (options.cache_subsystem) try sources.append(ft_root ++ "/src/cache/ftcache.c");
+    if (options.gzip) try sources.append(ft_root ++ "/src/gzip/ftgzip.c");
+    if (options.lzw) try sources.append(ft_root ++ "/src/lzw/ftlzw.c");
+    if (options.bzip2) try sources.append(ft_root ++ "/src/bzip2/ftbzip2.c");
+    if (options.gx_validation) try sources.append(ft_root ++ "/src/gxvalid/gxvalid.c");
+    if (options.opentype_validation) try sources.append(ft_root ++ "/src/otvalid/otvalid.c");
+    if (options.psaux) try sources.append(ft_root ++ "/src/psaux/psaux.c");
+    if (options.pshinter) try sources.append(ft_root ++ "/src/pshinter/pshinter.c");
+    if (options.psnames) try sources.append(ft_root ++ "/src/psnames/psnames.c");
+    if (options.svg) try sources.append(ft_root ++ "/src/svg/ftsvg.c");
 
-    if (options.cache_subsystem) {
-        try sources.append(ft_root ++ "/src/cache/ftcache.c");
-    }
-
-    if (options.gzip) {
-        try sources.append(ft_root ++ "/src/gzip/ftgzip.c");
-    }
-
-    if (options.lzw) {
-        try sources.append(ft_root ++ "/src/lzw/ftlzw.c");
-    }
-
-    if (options.bzip2) {
-        try sources.append(ft_root ++ "/src/bzip2/ftbzip2.c");
-    }
-
-    if (options.gx_validation) {
-        try sources.append(ft_root ++ "/src/gxvalid/gxvalid.c");
-    }
-
-    if (options.opentype_validation) {
-        try sources.append(ft_root ++ "/src/otvalid/otvalid.c");
-    }
-
-    if (options.psaux) {
-        try sources.append(ft_root ++ "/src/psaux/psaux.c");
-    }
-
-    if (options.pshinter) {
-        try sources.append(ft_root ++ "/src/pshinter/pshinter.c");
-    }
-
-    if (options.psnames) {
-        try sources.append(ft_root ++ "/src/psnames/psnames.c");
-    }
-
-    if (options.svg) {
-        try sources.append(ft_root ++ "/src/svg/ftsvg.c");
-    }
-
-    if (options.raster) {
-        try sources.append(ft_root ++ "/src/raster/raster.c");
-    }
-
-    if (options.sdf) {
-        try sources.append(ft_root ++ "/src/sdf/sdf.c");
-    }
-
-    if (options.smooth) {
-        try sources.append(ft_root ++ "/src/smooth/smooth.c");
-    }
+    if (options.raster) try sources.append(ft_root ++ "/src/raster/raster.c");
+    if (options.sdf) try sources.append(ft_root ++ "/src/sdf/sdf.c");
+    if (options.smooth) try sources.append(ft_root ++ "/src/smooth/smooth.c");
 
     const detected_target = (try std.zig.system.NativeTargetInfo.detect(b.allocator, target)).target;
 
@@ -241,12 +158,10 @@ fn buildFreeType(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.
         try sources.append(ft_root ++ "/src/base/ftsystem.c");
         try sources.append(ft_root ++ "/src/base/ftdebug.c");
     }
-
     if (detected_target.os.tag.isBSD() or detected_target.os.tag == .linux) {
         lib.defineCMacro("HAVE_UNISTD_H", "1");
         lib.defineCMacro("HAVE_FCNTL_H", "1");
         try sources.append(ft_root ++ "/builds/unix/ftsystem.c");
-
         if (detected_target.os.tag == .macos) {
             try sources.append("/src/base/ftmac.c");
         }
